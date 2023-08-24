d331444a646712fb9b8b5b4ee3262c15e7109eaf
ritik kumar <sinharitik589>
2023-08-25 02:11:29 +0530

    added breakpoint

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -27,15 +27,19 @@ export default function App() {
   },[])
 
   return (
+    <>
+    <div className="text-4xl text-gray-500 ph-cont vh-100 vw-100 text-center">Please open in desktop</div>
     <div className="main-cont">
-       <Nav/>
-      <div className="container bg-gray-main mx-auto sm:p-3">
-        <Router>
-        <Home path="/" />
-        <Dashboard path="/dash/:type?" />
-        </Router>
-      </div>
-      <Footer/>
-    </div>
+        
+        <Nav/>
+       <div className="container bg-gray-main mx-auto sm:p-3">
+         <Router>
+         <Home path="/" />
+         <Dashboard path="/dash/:type?" />
+         </Router>
+       </div>
+       <Footer/>
+     </div>
+    </>
   )
 }
